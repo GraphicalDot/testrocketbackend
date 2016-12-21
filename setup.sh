a8aded17dcdfd4fd557046383e875aa1ebf0bf6e
kaali <houzier.saurav@gmail.com>
2016-12-21 17:17:52 +0530

    Installation done

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+
+
+sudo apt-get install python-dev python-pip python-lxml python-cffi libcairo2 libpango1.0-0 libgdk-pixbuf2.0-0 shared-mime-info
+sudo apt-get install libxml2-dev libxslt1-dev python-dev
+sudo apt-get install libxml2-dev libxslt-dev python-dev lib32z1-dev libffi-dev
